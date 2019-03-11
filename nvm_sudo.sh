084ca0a940b16210f8603c362d660bd7443fc4bb
Aditya Chatterjee <a.chatterjee@f5.com>
2019-03-11 07:12:51 +0000

    comments

--- nvm_sudo.sh
+++ nvm_sudo.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+#when using nvm, the 'node' command points to a different location depending on the active version, which makes 'sudo node' fail. this script copies the active version of node into /usr/local
+
 n=$(which node); 
 n=${n%/bin/node}; 
 chmod -R 755 $n/bin/*; 
